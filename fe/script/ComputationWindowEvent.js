d033196f491f1209fe55ac44eaf9cf146ecde5ec
Stefan Sidlovsky <xsidlovs@fi.muni.cz>
2023-10-09 18:39:13 +0200

    created computation window

---
+++ fe/script/ComputationWindowEvent.js
@@ -0,0 +1,35 @@
+// Listen for 'start-computation' event and start computation from (this) new window
+TAURI.event.listen('start-computation', (event) => {
+    const aeonString = event.payload['aeonString']
+
+    const windowSessionKey = TAURI.window.getCurrent().label;
+    Session.createWindowSession(windowSessionKey)
+
+    Computation.startComputation(aeonString, windowSessionKey)
+});
+
+
+// Destroy computation session when window is closed
+TAURI.window.getCurrent().listen("tauri://close-requested", async () => {
+
+    // If computation is still running, ask user to confirm
+    if (await Session.hasRunningComputation()) {
+        const closeWindowWithRunningComputation = await TAURI.dialog.ask(Strings.runningComputation, {
+            title: 'Running computation',
+            type: 'warning',
+        });
+        if (!closeWindowWithRunningComputation) {
+            // User decided to not close the window with running computation -> do nothing
+            return
+        }
+    }
+
+    Session.destroyWindowSession()
+        .then(async (okMessage) => {
+            const currentWindow = TAURI.window.getCurrent();
+            await currentWindow.close();
+        })
+        .catch((errorMessage) => {
+            MessageDialog.errorMessage(errorMessage)
+        })
+})
