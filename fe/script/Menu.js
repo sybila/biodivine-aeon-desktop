63e959598305719223c383e36a07dfb1fe89838a
Stefan Sidlovsky <xsidlovs@fi.muni.cz>
2023-10-09 18:44:50 +0200

    implemented menu for new windows

---
+++ fe/script/Menu.js
@@ -0,0 +1,40 @@
+// Listen to the window menu item click and decide action
+TAURI.window.appWindow.onMenuClicked(({ payload: menuItemId }) => {
+    switch (menuItemId) {
+        case 'help':
+            UI.openHelpWindow()
+            break
+        case 'local_storage':
+            LiveModel.loadFromLocalStorage()
+            break
+        case 'aeon_import':
+            UI.importAeon()
+            break
+        case 'sbml_import':
+            UI.importSBML()
+            break
+        case 'g2a':
+            LiveModel.handleAeonModelImport(Examples.g2a)
+            break
+        case 'g2b':
+            LiveModel.handleAeonModelImport(Examples.g2b)
+            break
+        case 'budding_yeast_orlando':
+            LiveModel.handleAeonModelImport(Examples.buddingYeastOrlando)
+            break
+        case 'budding_yeast_irons':
+            LiveModel.handleAeonModelImport(Examples.buddingYeastIrons)
+            break
+        case 'aeon_export':
+            UI.downloadAeon()
+            break
+        case 'sbml_export_parametrized':
+            UI.downloadSBML()
+            break
+        case 'sbml_export_instantiated':
+            UI.downloadSBMLInstantiated()
+            break
+        default:
+            console.log("Unknown menu item id")
+    }
+});
